ef594cfa1e5040a883431958f1b50b78469e6213
Rodrigo <dc.4231253@gmail.com>
2026-07-28 02:46:07 -0500

    first commit

---
+++ src/main/java/pe/finanzas/finanzas/FinanzasApplication.java
@@ -0,0 +1,13 @@
+package pe.finanzas.finanzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanzasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/finanzas/finanzas/FinanzasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.finanzas.finanzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
